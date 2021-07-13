1a7e412f3e19a2ea0c4cf0852b404a63cd629208
Micah Godbolt <mgodbolt@microsoft.com>
2021-07-13 09:51:37 -0700

    fix exports

--- src/view/components/DiceRollerList.tsx
+++ src/view/components/DiceRollerList.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { v4 as uuid } from "uuid";
 import { DiceRoller } from "./DiceRoller";
-import { useGetDiceStore } from "../queries";
+import { useGetDiceStore } from "../store";
 
 export const DiceRollerList = () => {
   const {
